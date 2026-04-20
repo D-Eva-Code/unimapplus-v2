1be6a915c5b47f8da90d657f500794b8a8e04a82
Osahon <amiensosahon9@gmail.com>
2026-04-20 21:35:43 +0100

    login logo fix

--- unimapplus/frontend/src/pages/Login.jsx
+++ unimapplus/frontend/src/pages/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import api from "../services/api";
-import logo from "./";
 
 const ROLES = [
   {
